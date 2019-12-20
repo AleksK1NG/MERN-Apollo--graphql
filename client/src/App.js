0169ada56e186ca9bb81c61364b4026d7212b38c
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-20 14:44:15 +0300

    add home, register, login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
+
+const LoginPage = React.lazy(() => import('./Pages/LoginPage/LoginPage'))
+const RegisterPage = React.lazy(() => import('./Pages/RegisterPage/RegisterPage'))
+const HomePage = React.lazy(() => import('./Pages/HomePage/HomePage'))
 
 const App = () => {
   return (
-    <div>
-      <h1>React =D</h1>
-    </div>
+    <>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/register" component={RegisterPage} />
+          </Switch>
+        </Router>
+      </Suspense>
+    </>
   )
 }
 
