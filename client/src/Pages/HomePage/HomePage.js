ed6551aaa852692d1bbd74a886da98282e21177c
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-20 16:06:06 +0300

    add PostCards to home page

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { FETCH_POSTS_QUERY } from './homePageQuery'
 
+import { Grid } from 'semantic-ui-react'
+import PostCard from '../../components/Post/PostCard'
+
 const HomePage = () => {
   const { loading, data, error } = useQuery(FETCH_POSTS_QUERY)
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error</p>
 
-  if (data) {
-    console.log('data => ', data?.getAllPosts)
-  }
+  const posts = data?.getAllPosts
 
   return (
-    <div>
-      <h2 style={{ paddingTop: '100px' }}>Home page 🚀</h2>
-      {data && data?.getAllPosts.length}
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   )
 }
 
