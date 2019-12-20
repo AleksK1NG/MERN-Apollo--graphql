a43e283c62a5e34bdfb39b533089f2c7662d6c97
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-20 15:58:18 +0300

    add HomePage getAllPosts query

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { FETCH_POSTS_QUERY } from './homePageQuery'
 
 const HomePage = () => {
+  const { loading, data, error } = useQuery(FETCH_POSTS_QUERY)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+
+  if (data) {
+    console.log('data => ', data?.getAllPosts)
+  }
+
   return (
     <div>
-      <h2>Home page</h2>
+      <h2 style={{ paddingTop: '100px' }}>Home page 🚀</h2>
+      {data && data?.getAllPosts.length}
     </div>
   )
 }
