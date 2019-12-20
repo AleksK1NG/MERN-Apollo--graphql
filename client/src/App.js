48b04d9bab5965392560f4ebfd9cf74cc2d30c2b
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-20 15:30:46 +0300

    add MenuBar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
+import MenuBar from './components/Shared/MenuBar'
+import { Container } from 'semantic-ui-react'
 
 const LoginPage = React.lazy(() => import('./Pages/LoginPage/LoginPage'))
 const RegisterPage = React.lazy(() => import('./Pages/RegisterPage/RegisterPage'))
@@ -12,11 +14,14 @@ const App = () => {
     <>
       <Suspense fallback={<p>Loading...</p>}>
         <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/register" component={RegisterPage} />
-          </Switch>
+          <Container>
+            <Switch>
+              <MenuBar />
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/register" component={RegisterPage} />
+            </Switch>
+          </Container>
         </Router>
       </Suspense>
     </>
