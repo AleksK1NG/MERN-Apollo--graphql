95cb8a984e72e52fda9d7847658c803f67f8a74e
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-21 17:15:01 +0300

    add LikeButton and like post mutation

---
+++ client/src/components/LikeButton/LikeButton.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/react-hooks'
+import { Button, Label, Icon } from 'semantic-ui-react'
+import { LIKE_POST_MUTATION } from './likePostMutation'
+
+const LikeButton = ({ user, post }) => {
+  const { id, likeCount, likes } = post
+
+  const [liked, setLiked] = useState(false)
+
+  useEffect(() => {
+    if (user && likes.find((like) => like.username === user.username)) {
+      setLiked(true)
+    } else {
+      setLiked(false)
+    }
+  }, [user, likes])
+
+  const [likePost, { loading }] = useMutation(LIKE_POST_MUTATION, {
+    variables: { postId: id },
+    onError(err) {
+      console.error(err)
+    },
+  })
+
+  return (
+    <Button as="div" labelPosition="right" onClick={likePost}>
+      {user ? (
+        liked ? (
+          <Button color="teal" disabled={loading}>
+            <Icon name="heart" />
+          </Button>
+        ) : (
+          <Button color="teal" basic disabled={loading}>
+            <Icon name="heart" />
+          </Button>
+        )
+      ) : (
+        <Button as={Link} to="/login" color="teal" basic disabled={loading}>
+          <Icon name="heart" />
+        </Button>
+      )}
+      <Label basic color="teal" pointing="left">
+        {likeCount}
+      </Label>
+    </Button>
+  )
+}
+
+export default LikeButton
