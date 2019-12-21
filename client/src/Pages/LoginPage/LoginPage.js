674f105c979be9d31dc7fbd6cf5ca8e585938b90
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-21 13:00:56 +0300

    add login page and login mutation

--- client/src/Pages/LoginPage/LoginPage.js
+++ client/src/Pages/LoginPage/LoginPage.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from '../../hooks/useForm'
+import { useMutation } from '@apollo/react-hooks'
+import { LOGIN_USER } from './loginPageMutations'
+import { Button, Form } from 'semantic-ui-react'
+import { useHistory } from 'react-router-dom'
+
+const initialState = {
+  email: '',
+  password: '',
+}
 
 const LoginPage = () => {
+  const history = useHistory()
+  const [errors, setErrors] = useState({})
+  const { values, onChange, onSubmit, resetForm } = useForm(loginUser, initialState)
+  const [userLogin, { loading, error }] = useMutation(LOGIN_USER, {
+    update(proxy, result) {
+      console.log(result)
+      history.push('/')
+    },
+    variables: values,
+    onError(err) {
+      console.error(err)
+      setErrors(err.graphQLErrors[0].extensions.exception.errors)
+    },
+  })
+
+  function loginUser() {
+    userLogin()
+    resetForm()
+  }
+
   return (
-    <div>
-      <h2>Login page</h2>
+    <div className="form-container">
+      <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
+        <h1>Login</h1>
+        <Form.Input
+          label="Email"
+          placeholder="Email.."
+          name="email"
+          type="email"
+          value={values.email}
+          error={errors.email ? true : false}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Password"
+          placeholder="Password.."
+          name="password"
+          type="password"
+          value={values.password}
+          error={errors.password ? true : false}
+          onChange={onChange}
+        />
+        <Button type="submit" primary disabled={loading}>
+          Login
+        </Button>
+      </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
