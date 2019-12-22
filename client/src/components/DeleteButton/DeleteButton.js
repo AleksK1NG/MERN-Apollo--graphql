e5f5670e09769a9ae6d26621c35a2f8b8c2319a1
Alexander <alexander.bryksin@yandex.ru>
2019-12-22 10:44:18 +0300

    add show confirm window on delete post mutation

--- client/src/components/DeleteButton/DeleteButton.js
+++ client/src/components/DeleteButton/DeleteButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useMutation } from '@apollo/react-hooks'
 import { Button, Confirm, Icon } from 'semantic-ui-react'
 import { DELETE_POST_MUTATION } from './deleteButtonMutation'
@@ -7,14 +7,14 @@ import { FETCH_POSTS_QUERY } from '../../Pages/HomePage/homePageQuery'
 const DeleteButton = ({ postId, callback }) => {
   const [confirmOpen, setConfirmOpen] = useState(false)
 
+  const handleConfirm = useCallback(() => setConfirmOpen((prev) => (prev = !prev)), [confirmOpen])
+
   const [deletePost, { loading }] = useMutation(DELETE_POST_MUTATION, {
     variables: { postId },
     update(proxy, result) {
       setConfirmOpen(false)
 
       const cache = proxy.readQuery({ query: FETCH_POSTS_QUERY })
-      // cache.getAllPosts = cache.getAllPosts.filter(post => post.id !== postId)
-      console.log('delete button cache => ', cache)
 
       proxy.writeQuery({
         query: FETCH_POSTS_QUERY,
@@ -28,10 +28,10 @@ const DeleteButton = ({ postId, callback }) => {
 
   return (
     <>
-      <Button as="div" color="red" floated="right" onClick={deletePost}>
+      <Button as="div" color="red" floated="right" onClick={handleConfirm}>
         <Icon name="trash" style={{ margin: 0 }} />
       </Button>
-      <Confirm open={confirmOpen} onCancel={() => setConfirmOpen(false)} onConfirm={deletePost} />
+      <Confirm open={confirmOpen} onCancel={handleConfirm} onConfirm={deletePost} />
     </>
   )
 }
